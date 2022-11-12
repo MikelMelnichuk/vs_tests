d35067582b2735d49ebf1c9ab9708e1b0b7643c7
Michael Melnichuk <mikel.melnichuk@gmail.com>
2022-11-12 14:43:08 +0200

    Practice returning different error-codes for different situations

--- fastapi2.py
+++ fastapi2.py
@@ -1,4 +1,19 @@
 
 from fastapi import FastAPI, Response, status
+from typing import Dict, Any
+
 
 app = FastAPI()
+
+
+@app.get('/blog/{id}', status_code=status.HTTP_404_NOT_FOUND)  # status_code is the default status-code returned 
+def blog_page(id: int, response: Response) -> Dict[str, Any]:
+    
+    if id > 5:
+        # Change the return status-code to "Not Found - 404"
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"message": f"Error No ID: {id} found!"}
+    
+    response.status_code = status.HTTP_200_OK
+    return {"message": f"Found {id}, default type: {type(id)}"}
+

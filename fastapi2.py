9d96514daa4278e423bcf3ab941861d4c04029b1
Michael Melnichuk <mikel.melnichuk@gmail.com>
2022-11-12 16:43:10 +0200

    Add functions with Summary and Description [Implicit and Explicit]

--- fastapi2.py
+++ fastapi2.py
@@ -1,6 +1,6 @@
 
 from fastapi import FastAPI, Response, status
-from typing import Dict, Any
+from typing import Dict, Any, Optional
 
 
 app = FastAPI()
@@ -30,3 +30,26 @@ def get_all_messages(id: int) -> Dict[str, Any]:
         )  # status_code is the default status-code returned 
 def get_message_comment(msg_id: int, comment_id: int) -> Dict[str, Any]:
     return {"message": f"Message {msg_id}, Comment: {comment_id}"}
+
+# Addition of a Summary and Description
+@app.get('/func1_with_documentation',
+         tags=['documentation'],
+         summary="This is the summary of func 1",
+         description="This is the description")  # This is the EXPLISIT description
+def func1_documentation(var1: int, var2: Optional[str] = None) -> Dict[str, Any]:
+    return {'message': f'Var1: {var1}, Var2: {var2}'}
+
+@app.get('/func2_with_documentation',
+         tags=['documentation'],
+         summary="This is the summary of func 2")
+def func2_documentation(var1: int, var2: Optional[str] = None) -> Dict[str, Any]:
+    """This description was taken from the DocString of the function in python [INPLICIT Description]
+
+
+    - var1 (int): integer variable given from the used
+    - var2 (Optional[str], optional): A string var like a name. Defaults to None.
+
+    Returns:
+        Dict[str, Any]: The result as a JSON string.
+    """
+    return {'message': f'Var1: {var1}, Var2: {var2}'}

e83d8274b39b0b67ee65951e9738729551b36dfd
Michael Melnichuk <mikel.melnichuk@gmail.com>
2022-11-12 12:27:58 +0200

    FastAPI - add GET with parameter

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ def index():
     return {"message": "Hello, Cyber!"}
 
 
+@app.get('/blog/{id}')
+def blog_page(id):
+    return {"message": f"This is blog with ID: {id}, default type: {type(id)}"}
+
+
 # To run:
 # uvicorn main:app --reload
 

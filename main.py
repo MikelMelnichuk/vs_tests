8e46495741e277e48c099b03c628a2c5158c89cf
Michael Melnichuk <mikel.melnichuk@gmail.com>
2022-11-12 13:43:05 +0200

    Add a vriable with only 3 possible valid valued GET + Enum

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ app = FastAPI()
 def index():
     return {"message": "Hello, Cyber!"}
 
+# FastAPI will try to match the path pattern one by one, in the oder the paths were defined/configured
 @app.get('/blog/all')
 def all_blog_page() -> Dict[str, Any]:
     return {"message": f"All blogs!"}
@@ -18,6 +19,15 @@ def all_blog_page() -> Dict[str, Any]:
 def blog_page(id: int) -> Dict[str, Any]:
     return {"message": f"This is blog with ID: {id}, default type: {type(id)}"}
 
+from enum import Enum
+class BlogType(str, Enum):
+    short = "short"
+    long = "long"
+    howto = "howto"
+
+@app.get('/blog/type/{blog_type}')
+def get_blog_type(blog_type: BlogType) -> Dict[str, Any]:
+    return {"message": f"This is blog is of type: {blog_type}, default type: {type(blog_type)}"}
 
 # To run:
 # uvicorn main:app --reload

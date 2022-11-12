d01e2e0107ff3868bd08c3f9a8b58cb02ce95979
Michael Melnichuk <mikel.melnichuk@gmail.com>
2022-11-12 13:32:47 +0200

    The order of the definitions is important, only this way both /blog/all and /blog/{id} will work

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ app = FastAPI()
 def index():
     return {"message": "Hello, Cyber!"}
 
+@app.get('/blog/all')
+def all_blog_page() -> Dict[str, Any]:
+    return {"message": f"All blogs!"}
 
 @app.get('/blog/{id}')
 def blog_page(id: int) -> Dict[str, Any]:

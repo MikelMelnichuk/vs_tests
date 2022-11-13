4712e95271b0904eb479fc653564b7dd45d34450
Michael Melnichuk <mikel.melnichuk@gmail.com>
2022-11-13 17:57:41 +0200

    Divide the project into sections, Add Classes in the POST request and Validation of the received data

---
+++ fastapi3_main.py
@@ -0,0 +1,13 @@
+
+
+from fastapi import FastAPI
+
+import fastapi3_routes_post
+
+
+app = FastAPI()
+app.include_router(fastapi3_routes_post.router)
+
+@app.get('/')
+def hello_world():
+    return {'message': 'Hello World!'}
---
+++ fastapi3_routes_post.py
@@ -0,0 +1,49 @@
+
+from typing import Optional
+
+
+from fastapi import APIRouter, Query, Body
+from pydantic import BaseModel
+
+router = APIRouter(
+    prefix='/blog',
+    tags=['blog']
+)
+
+class BlogModel(BaseModel):
+    title: str
+    content: str
+    published: Optional[bool]
+
+# This functin contains all 3 types of parameters:
+# 1) blog - Json data from the body of the request 
+# 2) id - This is a path parameter
+# 3) version - This is a Query parameter
+@router.post('/new/{id}')
+def create_blog(blog: BlogModel, id: int, version: int = 1):
+    return {
+        'id': id,
+        'version': version,
+        'blog': blog
+    }
+
+
+@router.post('/new/{id}/validators')
+def create_blog_validators(blog: BlogModel, id: int,
+                version: int = Query(default=None, 
+                                     title="Some Title for the parameter in Docs",
+                                     description="This is a description to Docs"
+                ),
+                testsss: str = Body(default=...,
+                                     min_length=5,
+                                     max_length=10,
+                                     regex="^[a-z ]*$"
+                )
+                    
+        ):
+    return {
+        'id': id,
+        'version': version,
+        'blog': blog,
+        'testsss': testsss
+    }

950a07218afab5a26335d1b446f0e31715403fcd
Michael Melnichuk <mikel.melnichuk@gmail.com>
2022-12-12 10:03:50 +0200

    The final games with the project

--- fastapi1.py
+++ fastapi1.py
@@ -1,4 +1,3 @@
-
 from typing import Dict, Any
 
 from fastapi import FastAPI
@@ -6,34 +5,43 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get('/')
+@app.get("/")
 def index():
     return {"message": "Hello, Cyber!"}
 
+
 # FastAPI will try to match the path pattern one by one, in the oder the paths were defined/configured
-@app.get('/blog/all')
+@app.get("/blog/all")
 def all_blog_page() -> Dict[str, Any]:
     return {"message": f"All blogs!"}
 
+
 from enum import Enum
+
+
 class BlogType(str, Enum):
     short = "short"
     long = "long"
     howto = "howto"
 
-# BTW: http://127.0.0.1:8000/docs#/default/get_blog_type_blog_type__blog_type__get 
-# Will provide ONLY the values that you can use for the function (very cool) 
-@app.get('/blog/type/{blog_type}')
+
+# BTW: http://127.0.0.1:8000/docs#/default/get_blog_type_blog_type__blog_type__get
+# Will provide ONLY the values that you can use for the function (very cool)
+@app.get("/blog/type/{blog_type}")
 def get_blog_type(blog_type: BlogType) -> Dict[str, Any]:
-    return {"message": f"This is blog is of type: {blog_type}, default type: {type(blog_type)}"}
+    return {
+        "message": f"This is blog is of type: {blog_type}, default type: {type(blog_type)}"
+    }
 
-# Query params are the 'additinal' parameters that are after the '?' and separated with a '&'
+
+# Query params are the 'additinal' parameters that are after the '?' and separated by a '&'
 # http://127.0.0.1:8000/blog/query_param_test?query_param1=123&query_param2=abc
-@app.get('/blog/query_param_test')
-def test_functin(query_param1=123, query_param2='abc'):
-    return {"message": f"query_param1: {query_param1}, query_param2: {query_param2}"}    
+@app.get("/blog/query_param_test")
+def test_functin(query_param1=123, query_param2="abc"):
+    return {"message": f"query_param1: {query_param1}, query_param2: {query_param2}"}
 
-@app.get('/blog/{id}')
+
+@app.get("/blog/{id}")
 def blog_page(id: int) -> Dict[str, Any]:
     return {"message": f"This is blog with ID: {id}, default type: {type(id)}"}
 
--- fastapi2.py
+++ fastapi2.py
@@ -1,4 +1,3 @@
-
 from fastapi import FastAPI, Response, status
 from typing import Dict, Any, Optional
 
@@ -6,42 +5,49 @@ from typing import Dict, Any, Optional
 app = FastAPI()
 
 
-@app.get('/blog/{id}', status_code=status.HTTP_404_NOT_FOUND)  # status_code is the default status-code returned 
+@app.get(
+    "/blog/{id}", status_code=status.HTTP_404_NOT_FOUND
+)  # status_code is the default status-code returned
 def blog_page(id: int, response: Response) -> Dict[str, Any]:
-    
+
     if id > 5:
         # Change the return status-code to "Not Found - 404"
         response.status_code = status.HTTP_404_NOT_FOUND
         return {"message": f"Error No ID: {id} found!"}
-    
+
     response.status_code = status.HTTP_200_OK
     return {"message": f"Found {id}, default type: {type(id)}"}
 
 
 # Addition of Tags, makes the '/docs' endpoint much easier to navigate
-@app.get('/messages/{id}',
-         tags=['messages']
-        )  # status_code is the default status-code returned 
+@app.get("/messages/{id}", tags=["messages"])
 def get_all_messages(id: int) -> Dict[str, Any]:
     return {"message": f"Message {id}, default type: {type(id)}"}
 
-@app.get('/messages/{msg_id}/comments/{comment_id}',
-         tags=['messages', 'comments']
-        )  # status_code is the default status-code returned 
+
+# Addition of Multiple Tags
+@app.get("/messages/{msg_id}/comments/{comment_id}", tags=["messages", "comments"])
 def get_message_comment(msg_id: int, comment_id: int) -> Dict[str, Any]:
     return {"message": f"Message {msg_id}, Comment: {comment_id}"}
 
-# Addition of a Summary and Description
-@app.get('/func1_with_documentation',
-         tags=['documentation'],
-         summary="This is the summary of func 1",
-         description="This is the description")  # This is the EXPLISIT description
-def func1_documentation(var1: int, var2: Optional[str] = None) -> Dict[str, Any]:
-    return {'message': f'Var1: {var1}, Var2: {var2}'}
 
-@app.get('/func2_with_documentation',
-         tags=['documentation'],
-         summary="This is the summary of func 2")
+# Addition of a Summary and Description
+@app.get(
+    "/func1_with_documentation",
+    tags=["documentation"],
+    summary="This is the summary of func 1",
+    description="This is the description",
+)  # This is the EXPLISIT description
+def func1_documentation(var1: int, var2: Optional[str] = None) -> Dict[str, Any]:
+    return {"message": f"Var1: {var1}, Var2: {var2}"}
+
+
+# Addition of a Summary and Description using the regular function documentation of python
+@app.get(
+    "/func2_with_documentation",
+    tags=["documentation"],
+    summary="This is the summary of func 2",
+)
 def func2_documentation(var1: int, var2: Optional[str] = None) -> Dict[str, Any]:
     """This description was taken from the DocString of the function in python [INPLICIT Description]
 
@@ -52,4 +58,4 @@ def func2_documentation(var1: int, var2: Optional[str] = None) -> Dict[str, Any]
     Returns:
         Dict[str, Any]: The result as a JSON string.
     """
-    return {'message': f'Var1: {var1}, Var2: {var2}'}
+    return {"message": f"Var1: {var1}, Var2: {var2}"}

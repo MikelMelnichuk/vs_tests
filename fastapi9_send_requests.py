b29bd3bba09148176f128bdd7de1b587e8a3e82a
Michael Melnichuk <mikel.melnichuk@gmail.com>
2022-12-14 13:45:53 +0200

    Add GET and POST sending examples via Python requests library

---
+++ fastapi9_send_requests.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    text: str
+
+
+@app.post("/")
+async def root(data: Item):
+    return {"message": f"You wrote: '{data.text}'"}
+
+
+# ------------------------------------------------------------------------------------------------------------
+
+import requests
+
+res = requests.post(
+    "http://localhost:8000/",
+    headers={
+        #'User-agent'  : 'Internet Explorer/2.0',
+        "Content-type": "application/json"
+    },
+    json={"text": "Fast API"},
+)
+# print(res.headers['content-type'])
+print(res.text)
+
+# Send get request to "fastapi8_what_to_test:app"
+url = "http://127.0.0.1:8000/items/foo"  # The Path-Variable 'item_id'=foo [withing the /items/ endpoint]
+x = requests.get(
+    url, headers={"X-Token": "coneofsilence"}
+)  # Query-Variable 'x_token'=coneofsilence
